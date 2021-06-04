894af6faeb9fce582f46672a39649866a4a6ae3d
Adrian <a.milewski@onet.eu>
2021-06-04 20:00:09 +0200

    Add dishes form

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Container, makeStyles, Typography} from "@material-ui/core";
+import {Container, Grow, makeStyles, Typography} from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
     container: {
@@ -15,20 +15,22 @@ const About = () => {
     const classes = useStyles()
 
     return (
-        <Container className={classes.container}>
-            <Typography variant="h4" gutterBottom={true}>About</Typography>
-            <div className={classes.content}>
-                <Typography variant={"body1"} component={"p"}>
-                    My name is Adrian. I study Computer Science at Warsaw University of Technology, in the Faculty of Electrical
-                    Engineering. I am a 2-nd year student. I have a lot of positive personality traits. Regarding to my
-                    diligence trait, it is worth mentioning that during the holidays I have been working on a various
-                    positions not related to the IT industry since 2015. I am passionate about coding in any form –
-                    from low-level languages like assembler to high-level like python. I do not have any working
-                    experience in this field, but some coding projects were made in recent years by me, which are
-                    shown in the “Projects” section.
-                </Typography>
-            </div>
-        </Container>
+        <Grow in>
+            <Container className={classes.container}>
+                <Typography variant="h4" gutterBottom={true}>About</Typography>
+                <div className={classes.content}>
+                    <Typography variant={"body1"} component={"p"}>
+                        My name is Adrian. I study Computer Science at Warsaw University of Technology, in the Faculty of Electrical
+                        Engineering. I am a 2-nd year student. I have a lot of positive personality traits. Regarding to my
+                        diligence trait, it is worth mentioning that during the holidays I have been working on a various
+                        positions not related to the IT industry since 2015. I am passionate about coding in any form –
+                        from low-level languages like assembler to high-level like python. I do not have any working
+                        experience in this field, but some coding projects were made in recent years by me, which are
+                        shown in the “Projects” section.
+                    </Typography>
+                </div>
+            </Container>
+        </Grow>
     );
 };
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,153 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {
+    Button,
+    Container,
+    Grow,
+    makeStyles,
+    MenuItem,
+    Paper,
+    Select,
+    Slider,
+    TextField,
+    Typography
+} from "@material-ui/core";
+import TimeField from 'react-simple-timefield';
+
+const useStyles = makeStyles(theme => ({
+    container: {
+        marginTop: theme.spacing(4),
+    },
+    paper: {
+        marginBottom: theme.spacing(3),
+        display: 'flex',
+        justifyContent: 'center',
+        padding: theme.spacing(2),
+    },
+    form: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        '& *': {
+            marginBottom: theme.spacing(1),
+        }
+    },
+    topLabel: {
+        fontWeight: 'bold',
+        marginBottom: theme.spacing(3)
+    },
+    dishType: {
+        marginBottom: theme.spacing(2),
+        width: '60%'
+    },
+    submitBtn: {
+        marginTop: theme.spacing(2)
+    }
+}))
 
 const Home = () => {
+    const classes = useStyles()
+    const [name, setName] = useState("")
+    const [duration, setDuration] = useState('00:00:00')
+    const [type, setType] = useState("pizza")
+
+    //optional
+    const [numberOfSlices, setNumberOfSlices] = useState()
+    const [diameter, setDiameter] = useState()
+
+    const[spicinessScale, setSpicinessScale] = useState(1)
+
+    const [slicesOfBread, setSlicesOfBread] = useState();
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        console.log('ez')
+    }
+
     return (
-        <div>
-            Home
-        </div>
+        <Grow in>
+            <Container maxWidth={"md"} className={classes.container}>
+                <Container maxWidth={"xs"}>
+                    <Paper className={classes.paper} elevation={5}>
+                        <form className={classes.form} onSubmit={handleSubmit}>
+                            <Typography variant={"h4"} className={classes.topLabel} gutterBottom={true}>Dishes!</Typography>
+                            <TextField
+                                label="name"
+                                variant="outlined"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                                required
+                            />
+                            <TimeField
+                                showSeconds
+                                value={duration}
+                                onChange={(e) => setDuration(e.target.value)}
+                                style={{width: 92}}
+                                input={<TextField label="Duration" value={duration} variant="outlined" />}
+                                required
+                            />
+                            <Select
+                                label={"Type"}
+                                value={type}
+                                onChange={(e => setType(e.target.value))}
+                                className={classes.dishType}
+                            >
+                                <MenuItem value={"pizza"}>Pizza</MenuItem>
+                                <MenuItem value={"soup"}>Soup</MenuItem>
+                                <MenuItem value={"sandwich"}>Sandwich</MenuItem>
+                            </Select>
+                            {type === "pizza" && (
+                                <>
+                                    <TextField
+                                        label={"Number of slices"}
+                                        type={"number"}
+                                        value={numberOfSlices}
+                                        onChange={(e) => setNumberOfSlices(e.target.value)}
+                                        required
+                                    />
+                                    <TextField
+                                        label={"Diameter"}
+                                        type={"number"}
+                                        value={diameter}
+                                        onChange={(e) => setDiameter(e.target.value)}
+                                        required
+                                    />
+                                </>
+                            )}
+                            {type === "soup" && (
+                                <>
+                                    <Typography id="scale-slider" gutterBottom>
+                                        Spiciness scale
+                                    </Typography>
+                                    <Slider
+                                        value={spicinessScale}
+                                        onChange={(event, newValue) => setSpicinessScale(newValue)}
+                                        getAriaValueText={() => spicinessScale}
+                                        aria-labelledby="scale-slider"
+                                        valueLabelDisplay="auto"
+                                        step={1}
+                                        marks
+                                        min={1}
+                                        max={10}
+                                    />
+                                </>
+                            )}
+                            {type === "sandwich" && (
+                                <TextField
+                                    label={"Slices of bread"}
+                                    type={"number"}
+                                    defaultValue={slicesOfBread}
+                                    required
+                                    value={slicesOfBread}
+                                    onChange={(e) => setSlicesOfBread(e.target.value)}
+                                />
+                            )}
+                            <Button color={"primary"} variant={"contained"} type="submit" className={classes.submitBtn}>Submit</Button>
+                        </form>
+                    </Paper>
+                </Container>
+            </Container>
+        </Grow>
     );
 };
 
